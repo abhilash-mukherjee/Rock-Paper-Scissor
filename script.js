386fe87d8deff9c2ca2b2be9036c4d8a04466b49
Abhilash Mukherjee <abhilash301267@gmail.com>
2022-08-13 19:09:42 +0530

    Gameplay now happens with buttons

--- script.js
+++ script.js
@@ -1,145 +1,96 @@
-const ROCK = "ROCK", PAPER = "PAPER", SCISSOR = "SCISSOR";
-main();
-function main()
+const Elements = Object.freeze({
+    ROCK: Symbol("ROCK"),
+    PAPER: Symbol("PAPER"),
+    SCISSOR: Symbol("SCISSOR")
+} )
+const Winner = Object.freeze({
+        PLAYER: Symbol("PLAYER"),
+        COMPUTER: Symbol("COMPUTER"),
+        NONE: Symbol("NONE")
+})
+alert("Let's start the game");
+const btns = document.querySelectorAll('button');
+const result = document.querySelector('#result');
+btns.forEach(btn => btn.addEventListener('click', onButtonClick));
+
+function onButtonClick(e)
 {
-    alert("Let's start the game");
-    game(displayOutputWithAlert,consoleLog,getPlayerChoiceThroughAlert);
+    let playerChoice = getPlayerChoiceFromButton(e);
+    let computerChoice = getComputerChoice();
+    let winner = playRound(playerChoice,computerChoice);
+    divLog(getRoundResultString(playerChoice,computerChoice,winner));
 }
 
-
-function consoleLog(message)
+function getRoundResultString(playerChoiceElement,computerChoiceElement,winner)
 {
-    console.log(message);
+    let playerChoice = getElementValue(playerChoiceElement);
+    let computerChoice = getElementValue(computerChoiceElement);
+    if(winner === Winner.PLAYER) return `You WON!!! ${playerChoice} beats ${computerChoice}`;
+    if(winner === Winner.COMPUTER) return `You LOST!!! ${computerChoice} beats ${playerChoice}`;
+    else return `Match DRAWN!!! Both chose ${playerChoice}`
 }
 
-function displayOutputWithAlert(output)
+function getPlayerChoiceFromButton(e)
 {
-    alert(output)
+    let choiceString = e.target.getAttribute('id').toUpperCase();
+    if(choiceString.localeCompare('ROCK') == 0)return Elements.ROCK;
+    if(choiceString.localeCompare('PAPER') == 0)return Elements.PAPER;
+    if(choiceString.localeCompare('SCISSOR') == 0)return Elements.SCISSOR;
+    else return undefined;
 }
 
-function game(displayOutput,log, getPlayerChoice){
-    let compScore =0,playerScore = 0;
-    for(i=0;i<5;i++)
-    {
-        let playerChoice = getPlayerChoice(log);
-        let computerChoice = getComputerChoice();
-        let roundResult = playRound(playerChoice,computerChoice,log);
-        if(!!roundResult === false)continue;
-        if(roundResult.includes("Won"))playerScore++;
-        else if(roundResult.includes("Lost"))compScore++;
-        displayOutput(roundResult);
-    }
-    let firstPart, secondPart;
-    if(playerScore>compScore){
-        firstPart = "You are WINNER. ";
-    }
-    else if(playerScore<compScore){
-        firstPart = "You lost the game. ";
-    }
-    else firstPart = "Match Drawn"
-    secondPart = `Your score ${playerScore}, Computer's score ${compScore}`;
-    displayOutput(firstPart + secondPart);
+function getElementValue(element)
+{
+    if(element === Elements.ROCK) return "ROCK";
+    if(element === Elements.PAPER) return "PAPER";
+    if(element === Elements.SCISSOR) return "SCISSOR";
+    return "NONE";
 }
+function getWinnertValue(element)
+{
+    if(element === Winner.PLAYER) return "PLAYER";
+    if(element === Winner.COMPUTER) return "COMPUTER";
+    return "NONE";
+}
+
+function divLog(message)
+{
+    result.textContent = message;
+}
+
 function getComputerChoice()
 {
     let choiceInt = getRandomInteger(3);
-    switch(choiceInt)
+    switch (choiceInt)
     {
         case 0:
-            return "ROCK"
-            break;
+            return Elements.ROCK;
         case 1:
-            return "PAPER"
-            break;
+            return Elements.PAPER;
         case 2:
-            return "SCISSOR"
-            break;
+            return Elements.SCISSOR;
     }
 }
-function getPlayerChoiceThroughAlert(log)
-{
-    let input = prompt("ROCK, PAPER or SCISSOR?");
-    log(`Input is ${input} of type ${typeof(input)}`);
-    return formatInput(input);
-}
-function formatInput(input)
-{
-    let tempInp = input.toUpperCase();
-    tempInp = tempInp.trim();
-    return tempInp;
-}
+
 function getRandomInteger(maxExclusive)
 {
     return Math.floor(Math.random() * maxExclusive);
 }
 
-function playRound(playerChoice,computerChoice,log)
+function playRound(playerChoice,computerChoice)
 {
-    if(!isPlayerInputValid(playerChoice))
+    if(playerChoice === computerChoice) return Winner.NONE;
+    switch(playerChoice)
     {
-        log("Incorrect Input. Try again");
-        return;
-    }
-    if(playerChoice.localeCompare(computerChoice) == 0)
-    {
-        return `Match DRAWN. Both you and computer picked ${computerChoice}`;
-    }
-    const WIN = "You Won! ", LOOSE = "You Lost! ";
-    let partOne,partTwo;
-    let winner,looser;
-    if(playerChoice.localeCompare(ROCK) == 0)
-    {
-        if(computerChoice.localeCompare(PAPER))
-        {
-            partOne = LOOSE;
-            winner = PAPER;
-            looser = ROCK;
-        }
-        else{
-            partOne = WIN;
-            winner = ROCK;
-            looser = SCISSOR;
-        }
-    }
-    else if(playerChoice.localeCompare(PAPER) == 0)
-    {
-        if(computerChoice.localeCompare(ROCK))
-        {
-            partOne = WIN;
-            winner = PAPER;
-            looser = ROCK;
-        }
-        else{
-            partOne = LOOSE;
-            winner = SCISSOR;
-            looser = PAPER;
-        }
-    }
-    else if(playerChoice.localeCompare(SCISSOR) === 0)
-    {
-        if(computerChoice.localeCompare(PAPER))
-        {
-            partOne = WIN;
-            winner = SCISSOR;
-            looser = PAPER;
-        }
-        else{
-            partOne = LOOSE;
-            winner = ROCK;
-            looser = SCISSOR;
-        }
-    }
-    partTwo = `${winner} beats ${looser}.`;
-    return partOne + partTwo;
-}
+        case Elements.ROCK:
+            if (computerChoice === Elements.PAPER) return Winner.COMPUTER;
+            else return Winner.PLAYER;
+        case Elements.PAPER:
+            if (computerChoice === Elements.SCISSOR) return Winner.COMPUTER;
+            else return Winner.PLAYER;
+        case Elements.SCISSOR:
+            if (computerChoice === Elements.ROCK) return Winner.COMPUTER;
+            else return Winner.PLAYER;
 
-function isPlayerInputValid(input)
-{
-    let formattedInp = formatInput(input);
-    let inputValidity = 
-        formattedInp.localeCompare(ROCK) === 0 || 
-        formattedInp.localeCompare(PAPER) === 0 ||
-        formattedInp.localeCompare(SCISSOR) === 0
-    ;
-    return inputValidity;
+    }
 }
